618fa49ab563294c2b60473a9b92df100767ba50
SrTrompetista <mateus.rocha19@gmail.com>
2017-04-18 22:13:41 -0300

    Forma o JSON bonitinho e não dá ponteiro nulo nos mappers.

--- src/main/java/dpi/pcap/mappers/IPMapper.java
+++ src/main/java/dpi/pcap/mappers/IPMapper.java
@@ -2,43 +2,26 @@ package dpi.pcap.mappers;
 
 import java.io.IOException;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Mapper;
 
 public class IPMapper extends Mapper<LongWritable, Text, Text, Text> 
 {
 	@Override
 	public void map(LongWritable key, Text record, Context context) throws IOException, InterruptedException 
-	{
-		Log log = LogFactory.getLog(IPMapper.class);
+	{	
+		//Configuration nos permite usar o contexto para armazenar valores.
+		Configuration conf = context.getConfiguration();	
 		
-		Configuration conf = context.getConfiguration();
+		String linha = record.toString();	
 		
-		String linha = record.toString();
-		log.info("IPMAPPER: Leu a linha: "+linha);
-		
-		if (linha.startsWith("ip.addr;"))
+		if (!linha.startsWith("ip.addr;"))	
 		{
-			conf.set("filtros", linha);
-			
-			String[] filtros = linha.split(";");
-			int i = 0;
-			
-			for (i = 0; i < filtros.length; i++)
-				if (filtros[i].equals("ip.src"))
-					break;
-			
-			conf.set("ip.src", Integer.toString(i));
-		}
-		
-		else
-		{
-			String  ipAddress = conf.get("ip.src");
-			String[] filtros = conf.get("filtros").split(";");
+			//Restaura a posição que "ip.src" está presente
+			String  ipAddress = conf.get("ip.src");		
+			String[] filtros = conf.get("filtrosIP").split(";");
 			String[] valores = linha.split(";");
 
 			StringBuilder strBuilder = new StringBuilder();
@@ -47,7 +30,7 @@ public class IPMapper extends Mapper<LongWritable, Text, Text, Text>
 			{
 				if (valores[i].isEmpty())
 				{
-					strBuilder.append(filtros[i]+"=vazio;");
+					//strBuilder.append(filtros[i]+"=vazio;");
 				} else
 				{
 					strBuilder.append(filtros[i]+"="+valores[i]+";");
@@ -56,7 +39,6 @@ public class IPMapper extends Mapper<LongWritable, Text, Text, Text>
 			}
 
 			//map: <key, value> -> <ip.src, ip.addr=172.16...;ip.checksum=0x0002;...>
-			log.info("IPMAPPER: Escreveu o par: <"+valores[Integer.parseInt(ipAddress)]+", "+strBuilder.toString()+">");
 			context.write(new Text(valores[Integer.parseInt(ipAddress)]),  new Text(strBuilder.toString()));
 		}	
 	}
--- src/main/java/dpi/pcap/reducers/IPReducer.java
+++ src/main/java/dpi/pcap/reducers/IPReducer.java
@@ -1,35 +1,29 @@
 package dpi.pcap.reducers;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.apache.hadoop.io.NullWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Reducer;
-import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
+import org.codehaus.jettison.json.JSONArray;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
 
 public class IPReducer extends Reducer<Text, Text, NullWritable, Text>
 { 
 	@Override
 	public void reduce(Text key, Iterable<Text> records, Context context) throws IOException, InterruptedException
 	{
-		Log log = LogFactory.getLog(IPReducer.class);
 
 		String chave, valor;
-		String valorExistente;
-		String[] valoresTotais, temp;
+		String[] temp;
 		JSONArray jsonExistente;
+		JSONObject jsonFinal = new JSONObject();
 
 		
-		Map<String, JSONArray> myMap2 = new HashMap<String, JSONArray>();
+		Map<String, JSONArray> mapChaveValores = new HashMap<String, JSONArray>();
 
 		for (Text record : records)
 		{
@@ -42,98 +36,35 @@ public class IPReducer extends Reducer<Text, Text, NullWritable, Text>
 				chave = temp[0];
 				valor = temp[1];
 
-				if (myMap2.containsKey(chave))
+				if (mapChaveValores.containsKey(chave))
 				{					
-					jsonExistente = myMap2.get(chave);
+					jsonExistente = mapChaveValores.get(chave);
 					jsonExistente.put(valor);
-					myMap2.put(chave, jsonExistente);
+					mapChaveValores.put(chave, jsonExistente);
 				} else
 				{
 					JSONArray list = new JSONArray();
 					list.put(valor);
-					myMap2.put (chave, list);
+					mapChaveValores.put (chave, list);
 				}
 			}
 		}
 		
-		Map<String, JSONObject> myMap3 = new HashMap<String, JSONObject>();
-		
-		String[] hierarquia;
-		for (Map.Entry<String, JSONArray> entry : myMap2.entrySet())
+		try 
 		{
-			chave = entry.getKey();
-			hierarquia = chave.split("\\.");
+			jsonFinal.put("key", key);
+			jsonFinal.put("protocol", "ip");
 			
-			for (int i = hierarquia.length -1; i >= 0; i--)
+			for (Map.Entry<String, JSONArray> entry : mapChaveValores.entrySet())
 			{
-				if (i == hierarquia.length-1)
-				{
-					try 
-					{
-						JSONObject novoJSON = new JSONObject();
-						novoJSON.put(hierarquia[i], entry.getValue());
-						myMap3.put(hierarquia[i], novoJSON);
-						log.info("i = "+i+". Criando novo JSON: "+hierarquia[i]+", "+novoJSON.toString());
-					} catch (JSONException e) 
-					{
-						e.printStackTrace();
-					}
-				} else if (i != hierarquia.length-1)
-				{
-					if (!myMap3.containsKey(hierarquia[i]))
-					{
-						try
-						{
-							log.info("Map não contém JSON "+hierarquia[i]);
-							JSONObject novoJSON = new JSONObject();
-							novoJSON.put(hierarquia[i], myMap3.get(hierarquia[i+1]));
-							myMap3.put(hierarquia[i], novoJSON);
-							myMap3.remove(hierarquia[i+1]);
-							log.info("i = "+i+". Criando novo JSON: "+hierarquia[i]+", "+novoJSON.toString());
-						} catch (JSONException e)
-						{
-							e.printStackTrace();
-						}
-					} else 
-					{	
-						try
-						{
-								log.info("Map contém JSON "+hierarquia[i]);
-								JSONObject novoJSON = new JSONObject();
-								
-								JSONObject mergedObj = new JSONObject();
-
-								Iterator i1 = myMap3.get(hierarquia[i]).keys();
-								Iterator i2 = myMap3.get(hierarquia[i+1]).keys();
-								
-								String tmp_key;
-								while(i1.hasNext()) {
-								    tmp_key = (String) i1.next();
-								    mergedObj.put(tmp_key, myMap3.get(hierarquia[i]).get(tmp_key));
-								}
-								while(i2.hasNext()) {
-								    tmp_key = (String) i2.next();
-								    mergedObj.accumulate(tmp_key, myMap3.get(hierarquia[i+1]).get(tmp_key));
-								}
-								
-								
-								myMap3.put(hierarquia[i], mergedObj);
-								myMap3.remove(hierarquia[i+1]);
-								log.info("i = "+i+". Criando novo JSON: "+hierarquia[i]+", "+mergedObj.toString());
-								
-								
-						} catch (JSONException e)
-						{
-							e.printStackTrace();
-						}
-					}
-				}
+			    jsonFinal.put(entry.getKey(), entry.getValue());
 			}
+		} catch (JSONException e)
+		{
+			e.printStackTrace();
 		}
 		
-		JSONObject hue = myMap3.get("ip");
-		
-		context.write(NullWritable.get(), new Text(hue.toString()));
+		context.write(NullWritable.get(), new Text(jsonFinal.toString()));
 		
 	}
 }
